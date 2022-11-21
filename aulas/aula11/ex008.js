726edca2c0cc03d332a79c82c173436da8cc6d14
Diego Oliveira <diegonvnes@gmail.com>
2022-11-21 11:03:07 -0300

    exercicios da aula11

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel}Km/h.`)
+if (vel > 60) { //condição simples
+    console.log('Você ultrassou a velocidade permitida. Você foi multado.')
+}
+console.log('Dirija sempre usando sinto de segurança.')
---
+++ aulas/aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Você vive em ${país}.`)
+if (país == 'Brasil') { //condição composta
+    console.log('Você é brasileiro!')
+} else {
+    console.log('Você é estrangeiro!')
+}
