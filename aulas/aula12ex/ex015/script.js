09ebc9f5b2a059d199a3a4c61c89de489a128251
Diego Oliveira <diegonvnes@gmail.com>
2022-11-24 12:46:14 -0300

    correcao no ex015

--- aulas/aula12ex/ex015/script.js
+++ aulas/aula12ex/ex015/script.js
@@ -2,8 +2,7 @@ function carregar() {
     var msg = window.document.querySelector('div#msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 12
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 4 && hora < 12) {
         cump.innerHTML = 'Bom dia!'
