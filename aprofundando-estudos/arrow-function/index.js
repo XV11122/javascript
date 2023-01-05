b0abd022254065b3b16f530f0801c289fbe1d512
Diego Oliveira <diegonvnes@gmail.com>
2023-01-05 11:48:36 -0300

    organizacao das pastas  para melhor visualizacao

---
+++ aprofundando-estudos/arrow-function/index.js
@@ -0,0 +1,33 @@
+  /*  function sum(number1, number2) {
+        return number1 + number2
+    }
+
+    const sum = (number1, number2) => {
+        return number1 + number2
+    }
+
+    const sum = (number1, number2) => number1 + number2
+        console.log(sum(20, 20));
+
+    const community = () => 'CollabCode'
+        console.log(community())
+
+    const double = number => {
+        return number * 2;
+    }
+    console.log(double(20))
+
+    const sum = (year) => { 
+        if (year >= 18) {
+            return 'Maior de idade'
+        } else {
+            return 'Menor de idade'
+        }
+        }
+
+        console.log(sum(20))
+ */
+
+
+
+    
