6ac1fe555be7141465a638d98075c08876a4fe94
Diego Oliveira <diegonvnes@gmail.com>
2023-02-08 21:12:40 -0300

    ex002 em funcoes

---
+++ aprofundando-estudos/funcoes/ex002/script.js
@@ -0,0 +1,12 @@
+const idadeDiego = 19
+const idadeJuliana = 28
+
+const idadeDirce = 56
+const idadeRobert = 22
+
+function sum(number1, number2) {
+    console.log(number1 + number2)
+}
+
+sum(idadeDiego, idadeJuliana)
+sum(idadeDirce, idadeRobert)
